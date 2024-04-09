106df64c6b063b582abe4a886d66310268477977
Raj Patel <rajhgsp@gmail.com>
2024-04-09 04:21:18 -0700

    git fucked up and I had to make a new branch

--- audio.py
+++ audio.py
@@ -1,3 +1,190 @@
+# # !pip install -U openai-whisper
+# # !pip install librosa
+# # !pip install torchaudio
+
+# ### IMPORTS 
+
+# import warnings
+# warnings.filterwarnings("ignore", category=UserWarning)
+# warnings.filterwarnings("ignore", category=FutureWarning)
+
+# import whisper
+# from transformers import pipeline
+# from transformers import AutoModelForAudioClassification
+# import librosa, torch
+# import pyaudio
+# import wave
+
+# def loadAudioSentimentModelBase():
+#     ### LOADING MODELS 
+#     # STT Model
+#     stt_model = whisper.load_model("base.en")
+#     return stt_model
+
+# def loadAudioSentimentModel():
+#     # Sentiment Analysis model
+#     '''
+#         More emotions however not a great accuracy
+#         Possible outputs:
+#             joy (or happiness): Indicating content that expresses joy, happiness, or pleasure.
+#             anger: Reflecting content that expresses anger, irritation, or frustration.
+#             fear: Signifying text that expresses fear, anxiety, or apprehension.
+#             sadness: Identifying content that expresses sadness, gloominess, or disappointment.
+#             disgust: Highlighting content that expresses disgust, revulsion, or contempt.
+#             surprise: For content that expresses surprise, amazement, or shock.
+#             neutral: Indicating content that doesn't strongly express any of the above emotions.
+#         Wasn't able to successfully replicate all
+
+#     '''
+#     sentiment_pipeline = pipeline("text-classification", model="nateraw/bert-base-uncased-emotion")
+#     return sentiment_pipeline
+
+
+# # def loadAudioToneModel():
+# #     # Tone analysis model
+# #     tone_model = AutoModelForAudioClassification.from_pretrained("3loi/SER-Odyssey-Baseline-WavLM-Categorical-Attributes")
+# #     return tone_model
+
+# # for fusion
+# def packageInputsForFusion(expressionLabel, confidence, command):
+#     arousel = 0
+#     valence = 0
+
+#     # 1 is low/negative and 3 is high/positive
+#     if expressionLabel == 'anger':
+#         arousel = 3
+#         valence = 1
+#     elif expressionLabel == 'disgust':
+#         arousel = 2
+#         valence = 1
+#     elif expressionLabel == 'fear':
+#         arousel = 3
+#         valence = 1
+#     elif expressionLabel == 'joy':
+#         arousel = 2
+#         valence = 3
+#     elif expressionLabel == 'sadness':
+#         arousel = 1
+#         valence = 1
+#     elif expressionLabel == 'surprise':
+#         arousel = 3
+#         valence = 3
+#     elif expressionLabel == 'neutral':
+#         arousel = 2
+#         valence = 2
+
+#     packagedValues = ('semantic', arousel, valence, confidence, command)
+#     return packagedValues
+
+# def runAudioRecognition(stt_model, sentiment_pipeline, results):
+#     ### COLLECTING AUDIO
+#     # Settings
+#     FORMAT = pyaudio.paInt16
+#     CHANNELS = 1
+#     RATE = 44100
+#     CHUNK = 1024
+#     RECORD_SECONDS = 5
+#     WAVE_OUTPUT_FILENAME = "output.wav"
+
+#     # Initialize pyaudio
+#     audio = pyaudio.PyAudio()
+
+#     # Start recording
+#     stream = audio.open(format=FORMAT, channels=CHANNELS,
+#                         rate=RATE, input=True,
+#                         frames_per_buffer=CHUNK)
+#     print("recording...")
+#     frames = []
+
+#     for i in range(0, int(RATE / CHUNK * RECORD_SECONDS)):
+#         data = stream.read(CHUNK)
+#         frames.append(data)
+#     print("finished recording")
+
+#     # Stop recording
+#     stream.stop_stream()
+#     stream.close()
+#     audio.terminate()
+
+#     # Save the recorded data as a WAV file
+#     wf = wave.open(WAVE_OUTPUT_FILENAME, 'wb')
+#     wf.setnchannels(CHANNELS)
+#     wf.setsampwidth(audio.get_sample_size(FORMAT))
+#     wf.setframerate(RATE)
+#     wf.writeframes(b''.join(frames))
+#     wf.close()
+
+#     audio_file = WAVE_OUTPUT_FILENAME
+
+#     ### Performing analysis 
+#     # STT
+#     transcription = stt_model.transcribe(audio_file)
+#     text = transcription["text"]
+
+#     # Sentiment Analysis
+#     print("performing analysis")
+#     sentiment = sentiment_pipeline(text)
+
+#     # # Tone Analysis
+#     # #get mean/std
+#     # mean = tone_model.config.mean
+#     # std = tone_model.config.std
+
+#     # raw_wav, _ = librosa.load(audio_file, sr=tone_model.config.sampling_rate)
+
+#     # #normalize the audio by mean/std
+#     # norm_wav = (raw_wav - mean) / (std+0.000001)
+
+#     # #generate the mask
+#     # mask = torch.ones(1, len(norm_wav))
+
+#     # #batch it (add dim)
+#     # wavs = torch.tensor(norm_wav).unsqueeze(0)
+
+#     # #predict
+#     # with torch.no_grad():
+#     #     pred = tone_model(wavs, mask)
+
+#     # #convert logits to probability
+#     # probabilities = torch.nn.functional.softmax(pred, dim=1)
+
+#     # # Find the index of the maximum value in the tensor
+#     # max_score_index = torch.argmax(pred, dim=1)
+#     # # Retrieve the corresponding label from model.config.id2label
+#     # max_label = tone_model.config.id2label[max_score_index.item()]
+#     # max_prob = probabilities[0, max_score_index.item()]
+
+#     # Print the results
+#     print(text)
+#     print(sentiment[0]['label'], "  ", sentiment[0]['score'])
+#     # print(max_label, "  ", max_prob.item())
+
+#     #TODO-ARU: extract commands here
+#     tricks = ["Stand", "Shake", "Turn", "Sit", "Bang"]
+#     command = ""
+
+#     for trick in tricks:
+#     # Check if the trick is in the input text
+#         if trick.lower() in text.lower():
+#             # Assign the trick to command
+#             command = trick
+
+#     results.append(packageInputsForFusion(sentiment[0]['label'], sentiment[0]['score'] * 100, command))
+#     #print(results)
+
+# # def main():
+# #     print("audio audio")
+# #     audioSentimentModelBase = loadAudioSentimentModelBase()
+# #     audioSentimentModel = loadAudioSentimentModel()
+# #     results = []
+# #     runAudioRecognition(audioSentimentModelBase, audioSentimentModel, results)
+# #     print("done")
+
+
+
+# # if __name__ == "__main__":
+# #     main()
+
 # !pip install -U openai-whisper
 # !pip install librosa
 # !pip install torchaudio
@@ -40,13 +227,13 @@ def loadAudioSentimentModel():
     return sentiment_pipeline
 
 
-# def loadAudioToneModel():
-#     # Tone analysis model
-#     tone_model = AutoModelForAudioClassification.from_pretrained("3loi/SER-Odyssey-Baseline-WavLM-Categorical-Attributes")
-#     return tone_model
+def loadAudioToneModel():
+    # Tone analysis model
+    tone_model = pipeline("audio-classification", model="ehcalabres/wav2vec2-lg-xlsr-en-speech-emotion-recognition")
+    return tone_model
 
 # for fusion
-def packageInputsForFusion(expressionLabel, confidence, command):
+def packageSemmanticInputsForFusion(expressionLabel, confidence, command):
     arousel = 0
     valence = 0
 
@@ -76,7 +263,37 @@ def packageInputsForFusion(expressionLabel, confidence, command):
     packagedValues = ('semantic', arousel, valence, confidence, command)
     return packagedValues
 
-def runAudioRecognition(stt_model, sentiment_pipeline, results):
+def packageToneInputsForFusion(expressionLabel, confidence):
+    arousel = 0
+    valence = 0
+
+    # 1 is low/negative and 3 is high/positive
+    if expressionLabel == 'anger':
+        arousel = 3
+        valence = 1
+    elif expressionLabel == 'disgust':
+        arousel = 2
+        valence = 1
+    elif expressionLabel == 'fear':
+        arousel = 3
+        valence = 1
+    elif expressionLabel == 'joy':
+        arousel = 2
+        valence = 3
+    elif expressionLabel == 'sadness':
+        arousel = 1
+        valence = 1
+    elif expressionLabel == 'surprise':
+        arousel = 3
+        valence = 3
+    elif expressionLabel == 'neutral':
+        arousel = 2
+        valence = 2
+
+    packagedValues = ('tone', arousel, valence, confidence)
+    return packagedValues
+
+def runAudioRecognition(stt_model, sentiment_pipeline, audioToneModel, results):
     ### COLLECTING AUDIO
     # Settings
     FORMAT = pyaudio.paInt16
@@ -159,6 +376,9 @@ def runAudioRecognition(stt_model, sentiment_pipeline, results):
     print(sentiment[0]['label'], "  ", sentiment[0]['score'])
     # print(max_label, "  ", max_prob.item())
 
+    tone = audioToneModel(audio_file)
+    print(tone[0]['label'], "  ", tone[0]['score'])
+
     #TODO-ARU: extract commands here
     tricks = ["Stand", "Shake", "Turn", "Sit", "Bang"]
     command = ""
@@ -169,18 +389,20 @@ def runAudioRecognition(stt_model, sentiment_pipeline, results):
             # Assign the trick to command
             command = trick
 
-    results.append(packageInputsForFusion(sentiment[0]['label'], sentiment[0]['score'] * 100, command))
+    results.append(packageSemmanticInputsForFusion(sentiment[0]['label'], sentiment[0]['score'] * 100, command))
+    results.append(packageToneInputsForFusion(tone[0]['label'], tone[0]['score'] * 100))
     #print(results)
 
-# def main():
-#     print("audio audio")
-#     audioSentimentModelBase = loadAudioSentimentModelBase()
-#     audioSentimentModel = loadAudioSentimentModel()
-#     results = []
-#     runAudioRecognition(audioSentimentModelBase, audioSentimentModel, results)
-#     print("done")
+def main():
+    print("audio audio")
+    audioSentimentModelBase = loadAudioSentimentModelBase()
+    audioSentimentModel = loadAudioSentimentModel()
+    audioToneModel = loadAudioToneModel()
+    results = []
+    runAudioRecognition(audioSentimentModelBase, audioSentimentModel, audioToneModel, results)
+    print("done")
 
 
 
-# if __name__ == "__main__":
-#     main()
+if __name__ == "__main__":
+    main()
--- game.py
+++ game.py
@@ -1,3 +1,289 @@
+# import cv2
+# # import exit
+# import threading
+# import numpy as np
+# import time
+# import keyboard
+# import sys, signal
+
+# import random
+
+# attentive = "./videos/Attentive.mov"
+# bang = "./videos/bang.mov"
+# confusion = "./videos/confusion.mov"
+# excited = "./videos/excited.mov"
+# happy = "./videos/happy.mov"
+# humanSad = "./videos/humanSad.mov"
+# idle = "./videos/idle.mov"
+# lieDown = "./videos/lieDown.mov"
+# paw = "./videos/paw.mov"
+# scared = "./videos/scared.mov"
+# sitSleep = './videos/sitSleep.mov'
+# standing = "./videos/standing.mov"
+# turn = "./videos/turn.mov"
+
+# # vid_names = [attentive, bang, confusion, excited, 
+# #              happy, humanSad, idle, lieDown, paw,
+# #              scared, sitSleep, standing, turn]
+
+# # attentive_video = []
+# # bang_video = []
+# # confusion_video = []
+# # excited_video = []
+# # happy_video = []
+# # humanSad_video = []
+# # idle_video = []
+# # lieDown_video = []
+# # paw_video = []
+# # scared_video = []
+# # sitSleep_video = []
+# # standing_video = []
+# # turn_video = []
+
+# # videos = [attentive_video, bang_video, confusion_video,
+# #           excited_video, happy_video, humanSad_video, 
+# #           idle_video, lieDown_video, paw_video, scared_video,
+# #           sitSleep_video, standing_video, turn_video]
+
+# # def gameInit():
+# #     for i, video in enumerate(videos):
+# #         cap = cv2.VideoCapture(vid_names[i]) 
+# #         if (cap.isOpened()== False): 
+# #             print("Error opening video file") 
+        
+# #         while(cap.isOpened()): 
+# #             ret, frame = cap.read() 
+# #             if ret == True: 
+# #                 videos[i].append(frame) 
+# #             else: 
+# #                 break
+
+# #         cap.release() 
+
+# # isPlaying = False
+
+
+
+# def playVideo(video):
+# # def playVideo(video, vid_name):
+    
+#     cap = cv2.VideoCapture(video) 
+#     if (cap.isOpened()== False): 
+#         print("Error opening video file") 
+    
+#     while(cap.isOpened()): 
+#         ret, frame = cap.read() 
+#         if ret == True: 
+#             cv2.imshow('Frame', frame) 
+        
+#             if cv2.waitKey(1) & 0xFF == ord('q'): 
+#                 break
+
+#         else: 
+#             break
+
+#     cap.release() 
+#     # setVideoPlaying(False)
+
+#     ''' may need to uncomment this on different computers'''
+#     # cv2.destroyAllWindows()
+
+#     # # cap = cv2.VideoCapture(vid_name)
+#     # for frame in video:
+#     #     # cap = cv2.VideoCapture(video) 
+#     #     # if (cap.isOpened()== False): 
+#     #     #     print("Error opening video file") 
+        
+#     #     # while(cap.isOpened()): 
+#     #     cv2.imshow('Playback', frame)
+#     #     if cv2.waitKey(1) & 0xFF == ord('q'): 
+#     #         break
+
+#     # # cap.release()
+     
+#     return
+
+# def valenceArousalToEmotion(valence, arousal):
+#     emotion = ""
+#     if(valence == 1 and arousal == 1):
+#         emotion = "sad"
+#     elif(valence == 1 and arousal == 2):
+#         emotion = "disgust"
+#     elif(valence == 1 and arousal == 3):
+#         emotion = "angry"
+#     elif(valence == 2 and arousal == 1):
+#         emotion = "low_neutral"
+#     elif(valence == 2 and arousal == 2):
+#         emotion = "neutral"
+#     elif(valence == 2 and arousal == 3):
+#         emotion = "attentive"
+#     elif(valence == 3 and arousal == 1):
+#         emotion = "happy"
+#     elif(valence == 3 and arousal == 2):
+#         emotion = "happy"
+#     elif(valence == 3 and arousal == 3):
+#         emotion = "surprise"
+#     return emotion
+
+# def playGame(arousal, valence, confidence, command):
+#     # if (confidence < 0.3):
+#     if (confidence < 30):
+#         playVideo(confusion)
+#         # time.sleep(confusion_time)
+#     else:
+#         if(command == ""):
+#             emotion = valenceArousalToEmotion(valence, arousal)
+#             if(emotion == "sad" or emotion == "disgust"):
+#                 playVideo(humanSad)
+#                 # time.sleep(humanSad_time)
+#             elif(emotion == "angry"):
+#                 playVideo(scared)
+#                 # time.sleep(scared_time)
+#             elif(emotion == "neutral"):
+#                 playVideo(idle)
+#                 # time.sleep(idle_time)
+#             elif(emotion == "low_neutral"):
+#                 playVideo(lieDown)
+#                 # time.sleep(lieDown_time)
+#             elif(emotion == "attentive"):
+#                 playVideo(attentive)
+#                 # time.sleep(attentive_time)
+#             elif(emotion == "happy"):
+#                 playVideo(happy)
+#                 # time.sleep(happy_time)
+#             elif(emotion == "surprise"):
+#                 playVideo(excited)
+#                 # time.sleep(excited_time)
+#             # else:
+#             #     playVideo(idle)
+            
+#         else:
+#             if(command.lower() == "stand"):
+#                 playVideo(standing)
+#                 # time.sleep(standing_time)
+#             elif(command.lower() == "shake"):
+#                 playVideo(paw)
+#                 # time.sleep(paw_time)
+#             elif(command.lower() == "turn"):
+#                 playVideo(turn)
+#                 # time.sleep(turn_time)
+#             elif(command.lower() == "sit"):
+#                 playVideo(sitSleep)
+#                 # time.sleep(sitSleep_time)
+#             elif(command.lower() == "bang"):
+#                 playVideo(bang)
+#                 # time.sleep(bang_time)
+
+# def runGame(arousal, valence, confidence, command):
+#     print("(" + str(arousal) + ", " + str(valence) + ", " + str(confidence) + ", " + command + ")")
+#     playGame(arousal, valence, confidence, command)
+
+# def handleInput():
+#     global exitFlag
+#     while(True):
+#         # user_input = await asyncio.to_thread(input, "Enter 'Return' to stop: ")
+#         userInput = input("Enter 'q' to stop: \n")
+#         if userInput.lower() == 'q':
+#             print("Exiting!\n")
+#             exitFlag = True
+#             break
+
+# # def setVideoPlaying(playing):
+# #     global isPlaying 
+# #     isPlaying = playing
+# #     return
+
+# # def getVideoPlaying():
+# #     return isPlaying
+
+
+
+
+# ######################### TESTING ##########################
+
+# import datetime
+# def humanTime():
+#     return (datetime.datetime.fromtimestamp(time.time())).strftime('%Y-%m-%d %H:%M:%S')
+
+
+# import asyncio
+# exitFlag = False
+
+# # async def runGame():
+# def runTestGame():
+#     while(exitFlag == False):
+#         time.sleep(5)
+#         print("end of input: " + humanTime())
+#         valence = random.randint(1, 3)
+#         arousal = random.randint(1, 3)
+#         confidence = random.uniform(0, 1) * 100
+#         command = "stand"
+#         print("(" + str(arousal) + ", " + str(valence) + ", " + str(confidence) + ", " + command + ")")
+#         playGame(arousal, valence, confidence, command)
+#         print("end of video: " + humanTime())
+#         # await asyncio.sleep(1)
+
+# # async def handleInput():
+# def handle_Input():
+#     global exitFlag
+#     while(True):
+#         # user_input = await asyncio.to_thread(input, "Enter 'Return' to stop: ")
+#         userInput = input("Enter 'q' to stop: \n")
+#         if userInput.lower() == 'q':
+#             print("Exiting!\n")
+#             exitFlag = True
+#             break
+
+# # async def tester():
+# def tester():
+#     # exitThread = threading.Thread(target=exit.checkExit(), args=None)
+#     # exitThread.start()
+#     # while(exit.getExit()):
+
+#     # gameInit()
+
+#     # try:
+#         # while(True):
+#         #     time.sleep(5)
+#         #     print("end of input: " + humanTime())
+#         #     valence = random.randint(1, 3)
+#         #     arousal = random.randint(1, 3)
+#         #     confidence = random.uniform(0, 1) * 100
+#         #     command = "stand"
+#         #     print("(" + str(arousal) + ", " + str(valence) + ", " + str(confidence) + ", " + command + ")")
+#         #     playGame(arousal, valence, confidence, command)
+#         #     print("end of video: " + humanTime())
+#     # except input() == "n":
+#     #     print('exit')
+
+#     # exitThread.join()
+
+#     # for frame in attentive_video:
+
+#     # Create tasks for the loop and input handling
+#     # loop_task = asyncio.create_task(runGame())
+#     # input_task = asyncio.create_task(handleInput())
+
+#     # # Wait for either task to finish
+#     # await asyncio.wait([loop_task, input_task], return_when=asyncio.FIRST_COMPLETED)
+
+#     # # Cancel the remaining task
+#     # if not loop_task.done():
+#     #     loop_task.cancel()
+#     # if not input_task.done():
+#     #     input_task.cancel()
+
+#     inputThread = threading.Thread(target=handle_Input)
+#     inputThread.start()
+
+#     runTestGame()
+
+#     inputThread.join()
+
+# # asyncio.run(tester())
+
+# # tester()
+
 import cv2
 # import exit
 import threading
